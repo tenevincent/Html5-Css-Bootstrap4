31560c70f9f9626613cc09bcc21bc21e4c00244e
tenevincent <vincent.tene@gmail.com>
2020-06-02 22:52:55 +0200

    first commit

---
+++ Boostrap4/Intro-To-Boostrap4/IntroductionToBootstrap4/IntroductionToBootstrap4/Pages/Bootstrap/Grid_Clearfix.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntroductionToBootstrap4.Pages.Bootstrap
+{
+    public class Grid_ClearfixModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Boostrap4/Intro-To-Boostrap4/IntroductionToBootstrap4/IntroductionToBootstrap4/Pages/CSS3/Chapter01.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntroductionToBootstrap4.Pages.CSS3
+{
+    public class Chapter01Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
